22156d82dce2ae72d62e38e53f36f4c21e79eb74
gaowei1012 <gyb2020018@163.com>
2021-11-05 14:56:51 +0800

    add ui/checkbox ui

---
+++ ui/componets/Checkbox/index.tsx
@@ -0,0 +1,13 @@
+import { Checkbox, Text } from '@tarojs/components'
+import { QtCheckboxProps } from '../../types/CheckboxProps'
+
+const QtCheckbox = (props: QtCheckboxProps) => {
+  const { text } = props
+  return (
+    <Checkbox {...props} >
+      <Text style={{color: '#333'}}>{text}</Text>
+    </Checkbox>
+  )
+}
+
+export default QtCheckbox
---
+++ ui/types/CheckboxProps.d.ts
@@ -0,0 +1,8 @@
+import { CheckboxProps } from '@tarojs/components/types/Checkbox'
+
+export interface QtCheckboxProps extends CheckboxProps {
+  /** checkbox文本描述
+   * @default default
+   */
+  text?: string
+}
