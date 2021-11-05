4c30663c6c948409c68b7d5dc7e18b0e93654983
gaowei1012 <gyb2020018@163.com>
2021-11-05 19:58:51 +0800

    add card conponent

---
+++ src/componets/Card/index.tsx
@@ -0,0 +1,25 @@
+import { View } from '@tarojs/components'
+import { CardProps } from '../../../types/CardProps'
+import '../../styles/card.scss'
+
+const QtCard = (props: CardProps) => {
+  const { content, title, extra, onClick, customClass } = props
+  return (
+    <View className={`card_container ${customClass ? customClass : ''}`} onClick={onClick}>
+      <View className='card_header'>
+        <View className='card_header__left'>
+          {title}
+        </View>
+        {extra ? <View className='card_header__right'>
+          {extra}
+        </View> : <View />}
+      </View>
+      {title ? <View className='line' /> : ''}
+      <View className='card_content'>
+        {content}
+      </View>
+    </View>
+  )
+}
+
+export default QtCard
