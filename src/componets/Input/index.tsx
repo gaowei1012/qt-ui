0dcf004f67267cbdfb57c72cde1632cd3aa5cc8b
gaowei1012 <gyb2020018@163.com>
2021-11-05 22:39:45 +0800

    add input components

---
+++ src/componets/Input/index.tsx
@@ -0,0 +1,10 @@
+import { Input } from '@tarojs/components'
+import { QtInputProps } from '../../../types/InputProps'
+
+const QtInput = (props: QtInputProps) => {
+  return (
+    <Input {...props}/>
+  )
+}
+
+export default QtInput
---
+++ types/InputProps.d.ts
@@ -0,0 +1,5 @@
+import { InputProps } from '@tarojs/components/types/Input'
+
+export interface QtInputProps extends InputProps {
+
+}
